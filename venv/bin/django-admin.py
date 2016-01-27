2fd7da7552bd4701a0173fd3fc4ac2bba95d103d
Jma353 <jma353@cornell.edu>
2016-01-27 10:25:49 -0500

    Heroku w/o venv ignored

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/AntonakakisJ/Desktop/DesktopStuff/GitHub/Winter15/cuappsite/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
