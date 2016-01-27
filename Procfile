7fcc7abb34c66c392ef35e7e93f4066bf5a5f44e
Jma353 <jma353@cornell.edu>
2016-01-27 09:32:55 -0500

    Heroku setup start

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cuappsite.wsgi --log-file - 
--- cuappsite/settings.py
+++ cuappsite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url 
+DATABASES['default'] = dj_database_url.config()  
+DATABASES['default']['CONN_MAX_AGE'] = 500 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,16 +127,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 # Additional places to look for static assets 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
 REST_FRAMEWORK  = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'PAGE_SIZE': 10
 }
+
+
 
 
 
