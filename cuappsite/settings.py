72be84e1b9519abd8225364401e6d9a21374d927
Jma353 <jma353@cornell.edu>
2016-01-28 10:28:05 -0500

    Switch loader inclusion format in settings.py

--- cuappsite/settings.py
+++ cuappsite/settings.py
@@ -74,14 +74,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'loaders': [
-                ('django.template.loaders.cached.Loader', [
-                    'django.template.loaders.filesystem.Loader',
-                    'django.template.loaders.app_directories.Loader',
-                ]),
-            ],
         },
     },
+]
+
+TEMPLATE_LOADERS = [
+    'django.template.loaders.app_directories.Loader',
 ]
 
 
