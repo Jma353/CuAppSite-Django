c45f0f7cc5d6fd5d117b3d4d5aefbfa3652a3408
Jma353 <jma353@cornell.edu>
2016-01-29 19:43:26 -0500

    Consolidate admin-portal JS

--- static/js/admin/admin-portal.js
+++ static/js/admin/admin-portal.js
@@ -4,136 +4,7 @@ var applicantSelected = null;
 
 
 
-function traineeClicked(e, thisClicked) {
-
-	console.log('applicant selected'); 
-	thisClicked.addClass('applicantSelected'); 
-
-	if (applicantSelected != null) {
-		applicantSelected.removeClass('applicantSelected'); 
-	}
-
-	applicantSelected = thisClicked; 
-	var text = applicantSelected.text(); 
-	text = text.split('|')[1].trim().split('-')[0].trim(); 
-	console.log(text); 
-	var url = '/api/applicant/' + text + '.json'
-
-	console.log(url); 
-	$.ajax({
-		url: url, 
-		type: 'GET',
-		dataType: 'JSON',
-		success: function(json) {
-
-			console.log("Got here as well"); 
-			console.log(json); // JSON
-			$('.field').remove(); 
-			$('.sub-title').remove(); 
-
-
-			// Headline 
-			$('.info-section').append('<h2 class="field"><strong>Training Program Application</strong></h2>'); 
-
-			// Full name 
-			$('.info-section').append('<h4 class="field light-face">Name: ' 
-													+ json['first_name'] + ' ' + json['last_name'] + '</h4>')
-
-			// Email 
-			$('.info-section').append('<h4 class="field light-face">Email: '
-																+ json['email'] + '</h4>')
-
-			// Year 
-			$('.info-section').append('<h4 class="field light-face">Year: ' 
-																+ json['year'] + '</h4>')
-
-			// Major 
-			$('.info-section').append('<h4 class="field light-face">Major: '  
-																+ json['major'] + '</h4>')
-
-			// Highest CS Course 
-			$('.info-section').append('<h4 class="field light-face">Highest CS Course: '
-													+ json['trainee']['highest_cs_course'] + '</h4>')
-
-			// Essay 
-			$('.info-section').append('<h4 class="field light-face">Essay: </h4>')
-			$('.info-section').append('<p class="field light-face essay">' + json['trainee']['essay'] + '</p>')
-
-			// Portfolio Link 
-			$('.info-section').append('<h4 class="field light-face">Portfolio Link: <a>' 
-													+ json['trainee']['portfolio_link'] + '</a></h4>')
-
-			// Resume Link
-			$('.info-section').append('<h4 class="field light-face">Resume Link: <a>' 
-													+ json['trainee']['resume_link'] + '</a></h4>')
-
-
-			// Score input 
-			$('.info-section').append('<h4 class="field light-face">Score: <input class="form-control score" type="number" min="0" max="10" value="' 
-																					+ json['trainee']['score'] + '"></input></h4>'); 
-
-			// Status textarea 
-			$('.info-section').append('<h4 class="field light-face">Status: </h4>'); 
-			$('.info-section').append('<textarea class="form-control field status" maxlength="255"></textarea>'); 
-			$('.status').val(json['trainee']['status']); 
-
-			// Button to change score/status 
-			$('.info-section').append('<button class="red-link edit-trainee-app field">Edit score/status</button>'); 
-
-
-
-			// On-click function to submit data 
-			$('.edit-trainee-app').on('click', function(e) {
-
-				e.preventDefault(); 
-				
-
-				var text = applicantSelected.text(); 
-				var email = text.split('|')[1].trim().split('-')[0].trim(); 
-
-				console.log('Score: ' + $('.score').val()); 
-
-				console.log('Status: ' + $('.status').val()); 
-
-				var myJSON = {
-												'app': 'trainee',
-												'email': email,
-												'score': $('.score').val(),
-												'status': $('.status').val()
-										 }; 
-
-				var url = window.location.pathname; 
-
-				$.ajax({
-					url: url, 
-					type: 'POST', 
-					data: JSON.stringify(myJSON),
-					contentType: 'application/JSON; charset=utf-8',
-					dataType: 'JSON',
-					success: function(json) {
-						console.log("got here!"); 
-						if(json['redirect']) {
-							window.location.pathname = json['redirect']
-						}
-
-					}
-
-				}); 
-
-
-			});
-
-		},
-		error: function(xhr, err, errmsg) {
-			console.log("ERROR"); 
-		}
-
-	}); 
-}
-
-
-
-function candidateClicked(e, thisClicked) {
+function applicantClicked(e, thisClicked, typeOfApp) {
 
 	console.log('applicant selected');
 	thisClicked.addClass('applicantSelected'); 
@@ -161,7 +32,7 @@ function candidateClicked(e, thisClicked) {
 			$('.sub-title').remove(); 
 
 			// Headline 
-			$('.info-section').append('<h2 class="field"><strong>Core Team Application</strong></h2>'); 
+			$('.info-section').append('<h2 class="field"><strong>' + typeOfApp.toUpperCase() +' Application</strong></h2>'); 
 
 			// Full name 
 			$('.info-section').append('<h4 class="field light-face">Name: ' 
@@ -179,37 +50,43 @@ function candidateClicked(e, thisClicked) {
 			$('.info-section').append('<h4 class="field light-face">Major: '  
 																+ json['major'] + '</h4>')
 
-			// Role 
-			$('.info-section').append('<h4 class="field light-face">Role: '
-													+ json['candidate']['role'] + '</h4>')
+			if (typeOfApp == 'candidate') {
+				// Role 
+				$('.info-section').append('<h4 class="field light-face">Role: '
+														+ json[typeOfApp]['role'] + '</h4>')
+			} else { // 'trainee'
+				// Highest CS Course 
+				$('.info-section').append('<h4 class="field light-face">Highest CS Course: '
+														+ json[typeOfApp]['highest_cs_course'] + '</h4>')
+			}
 
 			// Essay 
 			$('.info-section').append('<h4 class="field light-face">Essay: </h4>')
-			$('.info-section').append('<p class="field light-face essay">' + json['candidate']['essay'] + '</p>')
+			$('.info-section').append('<p class="field light-face essay">' + json[typeOfApp]['essay'] + '</p>')
 
 			// Portfolio Link 
-			$('.info-section').append('<h4 class="field light-face">Portfolio Link: <a>' 
-													+ json['candidate']['portfolio_link'] + '</a></h4>')
+			$('.info-section').append('<h4 class="field light-face">Portfolio Link: </h4>')
+			$('.info-section').append('<p class="field light-face essay">' + json[typeOfApp]['portfolio_link'] + '</p>')
 
 			// Resume Link
-			$('.info-section').append('<h4 class="field light-face">Resume Link: <a>' 
-													+ json['candidate']['resume_link'] + '</a></h4>')
+			$('.info-section').append('<h4 class="field light-face">Resume Link: </h4>')
+			$('.info-section').append('<p class="field light-face essay">' + json[typeOfApp]['resume_link'] + '</p>')
 
 
 			// Score input 
 			$('.info-section').append('<h4 class="field light-face">Score: <input class="form-control score" type="number" min="0" max="10" value="' 
-																					+ json['candidate']['score'] + '"></input></h4>'); 
+																					+ json[typeOfApp]['score'] + '"></input></h4>'); 
 
 			// Status textarea 
 			$('.info-section').append('<h4 class="field light-face">Status: </h4>'); 
 			$('.info-section').append('<textarea class="form-control field status" maxlength="255"></textarea>'); 
-			$('.status').val(json['candidate']['status']); 
+			$('.status').val(json[typeOfApp]['status']); 
 
 			// Button to change score/status 
-			$('.info-section').append('<button class="red-link edit-candidate-app field">Edit score/status</button>'); 
+			$('.info-section').append('<button class="red-link edit-' + typeOfApp + '-app field">Edit score/status</button>'); 
 
 			// On-click function to submit data 
-			$('.edit-candidate-app').on('click', function(e) {
+			$('.edit-' + typeOfApp + '-app').on('click', function(e) {
 
 				e.preventDefault(); 
 				
@@ -222,7 +99,7 @@ function candidateClicked(e, thisClicked) {
 				console.log('Status: ' + $('.status').val()); 
 
 				var myJSON = {
-												'app': 'candidate',
+												'app': typeOfApp,
 												'email': email,
 												'score': $('.score').val(),
 												'status': $('.status').val()
@@ -261,7 +138,6 @@ function candidateClicked(e, thisClicked) {
 	});
 
 }
-
 
 
 
