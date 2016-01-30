b9ccfd7577fc2423f1b4de3d626fea52d02dd5c7
Jma353 <jma353@cornell.edu>
2016-01-29 19:08:27 -0500

    Remove venv from github

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/AntonakakisJ/Desktop/DesktopStuff/GitHub/Winter15/cuappsite/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
